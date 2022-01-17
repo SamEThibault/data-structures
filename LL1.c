a9543b0f6d13325f01678792087fd167f5b89e19
Samuel Emard-Thibault <92962712+SamEThibault@users.noreply.github.com>
2022-01-17 14:29:42 -0500

    Del functions to LL
    Added head and tail delete functions for LL.c, updated some comments and added gitignore for properties.json for local compiler path.

--- LL1.c
+++ LL1.c
@@ -1,5 +1,5 @@
 // Samuel Emard-Thibault - 01/05/2022
-// Basic singly linked list structure for general use with stack and queue functionalities
+// Basic singly linked list structure for general use with stack/queue/dequeue functionalities
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -54,7 +54,7 @@ pnode newNode(int a)
     return temp;
 }
 
-// adds argument node to front of argument list
+// adds argument node to front of argument list (dequeue)
 void addFront(pnode node, plist list)
 {
     // if list empty, both head and tail point to node
@@ -71,7 +71,7 @@ void addFront(pnode node, plist list)
     }
 }
 
-// adds argument nodee to the tail of argument list
+// adds argument node to the tail of argument list (stack/queue)
 void addBack(pnode node, plist list)
 {
     // same logic as previous function
@@ -88,6 +88,60 @@ void addBack(pnode node, plist list)
     }
 }
 
+// delete head node from list (queue)
+void delHead(plist list)
+{
+    // allocate memory for temp ptr
+    pnode ptr = (pnode)malloc(sizeof(pnode));
+
+    // if we got the memory: assign temp to the head of the list,
+    // update head to the next node, and free memory of previous head
+    if (ptr != NULL)
+    {
+        ptr = list->head;
+        int val = ptr->val; // store old head value to print
+        list->head = list->head->next;
+        free(ptr);
+
+        printf("Succesfully deleted head node containing value %d\n", val);
+    }
+    else
+    {
+        printf("ERROR: Unable to delete node.");
+    }
+}
+
+// delete tail node from list (stack)
+void delTail(plist list)
+{
+    // allocate memory for temp ptr
+    pnode ptr = (pnode)malloc(sizeof(pnode));
+
+    // if we got memory: assign temp to the head of the list
+    if (ptr != NULL)
+    {
+        ptr = list->head;
+
+        // increment ptr from head until it's pointing to the node BEFORE the tail node
+        while (ptr->next->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+
+        // store the value of the tail node before deleting, then free
+        int val = ptr->next->val;
+        free(ptr->next);
+        list->tail = ptr; // update list tail ptr
+        ptr->next = NULL; // set new tail ptr to point to NULL
+
+        printf("Succesfully deleted tail node containing value %d\n", val);
+    }
+    else
+    {
+        printf("ERROR: Unable to delete node.");
+    }
+}
+
 // routine to print list for testing
 void printList(plist list)
 {
@@ -96,20 +150,37 @@ void printList(plist list)
 
     while (temp != NULL)
     {
-        printf("%d", temp->val);
+        printf("%d -> ", temp->val);
         temp = temp->next;
     }
+
+    printf("NULL\n");
 }
 
 int main()
 {
+    // initialize list pointer
     plist list1 = newList();
 
+    // add elements to the tail of the specified list from 0 to the defined NUM
     for (int i = 0; i < NUM; i++)
     {
         addBack(newNode(i), list1);
     }
 
+    printList(list1);
+
+    // TESTING:
+
+    // test: add node to front and re-print list
     addFront(newNode(6), list1);
+    printList(list1);
+
+    // test front delete function then re-print list
+    delHead(list1);
+    printList(list1);
+
+    // test rear delete function then re-print list
+    delTail(list1);
     printList(list1);
 }
